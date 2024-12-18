c36cfaedb6d1bf41060773992580f163e78c9c84
Brain <brianjosephlesko@gmail.com>
2024-12-18 16:44:23 -0500

    faq

--- app/components/ButtonLogin.js
+++ app/components/ButtonLogin.js
@@ -1,15 +1,21 @@
 import Link from "next/link";
 
-const ButtonLogin = ({ isLoggedIn, name }) => {
+const ButtonLogin = ({ isLoggedIn, name, extraStyle }) => {
   if (isLoggedIn) {
     return (
-      <Link href="/dashboard" className="btn btn-primary">
+      <Link
+        href="/dashboard"
+        className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+      >
         Welcome back {name}
       </Link>
     );
   }
   return (
-    <Link href="/login" className="btn btn-primary">
+    <Link
+      href="/login"
+      className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+    >
       Login
     </Link>
   );
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import ButtonLogin from "./components/ButtonLogin";
+import FAQListItem from "./components/FAQListItem";
 
 export default function Home() {
   const isLoggedIn = true;
@@ -7,12 +8,12 @@ export default function Home() {
   return (
     <main>
       {/* Header */}
-      <section className="bg-gray-100">
+      <section className="bg-gray-100" id="header">
         <div className="flex justify-between items-center px-8 py-2 max-w-3xl mx-auto">
           <div className="font-bold">Simplify My ERP</div>
           <div className="space-x-4 max-md:hidden">
-            <a className="link link-hover">Products</a>
-            <a className="link link-hover">Pricing</a>
+            <a className="link link-hover" href="#products">Products</a>
+            <a className="link link-hover" href="#pricing">Pricing</a>
           </div>
           <div>
             <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
@@ -20,7 +21,7 @@ export default function Home() {
         </div>
       </section>
       {/* Hero */}
-      <section className="text-center px-8 py-32 max-w-3xl mx-auto">
+      <section className="text-center px-8 py-32 max-w-3xl mx-auto" id="hero">
         <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
           Collect Customer Feedback to Build Better Products
         </h1>
@@ -31,7 +32,7 @@ export default function Home() {
         <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
       </section>
       {/* Pricing*/}
-      <section className="bg-base-200">
+      <section className="bg-base-200" id="pricing">
         <div className="px-8 py-32 max-w-3xl mx-auto">
           <p className="text-sm uppercase font-medium text-center text-primary mb-4">
             Pricing
@@ -48,79 +49,77 @@ export default function Home() {
               </div>
             </div>
 
+            {/* Features */}
             <ul className="space-y-2">
-              <li className="flex gap-2 items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className=" text-green-600 size-4 "
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m4.5 12.75 6 6 9-13.5"
-                  />
-                </svg>
-                Up to 100 Employees
-              </li>
-              <li className="flex gap-2 items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className=" text-green-600 size-4 "
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m4.5 12.75 6 6 9-13.5"
-                  />
-                </svg>
-                PTO requests
-              </li>
-              <li className="flex gap-2 items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className=" text-green-600 size-4 "
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m4.5 12.75 6 6 9-13.5"
-                  />
-                </svg>
-                PTO Calendar
-              </li>
-              <li className="flex gap-2 items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className=" text-green-600 size-4 "
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m4.5 12.75 6 6 9-13.5"
-                  />
-                </svg>
-                Admin Dashboard
-              </li>
+              {[
+                "Up to 100 Employees",
+                "PTO requests",
+                "PTO Calendar",
+                "Admin Dashboard",
+              ].map((feature) => (
+                <li key={feature} className="flex items-center">
+                  <svg
+                    className="w-6 h-6 mr-2 text-green-600 size-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  {feature}
+                </li>
+              ))}
             </ul>
 
-            <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+            <ButtonLogin
+              isLoggedIn={isLoggedIn}
+              name={name}
+              extraStyle="w-full"
+            />
           </div>
+        </div>
+      </section>
+
+      {/* FAQ */}
+      <section className="bg-base-200" id='faq'>
+        <div className="px-8 py-32 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary mb-4">
+            FAQ
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+            Frequently Asked Questions
+          </h2>
+
+          <ul className="max-w-lg mx-auto ">
+            {
+              // FAQ items
+              [
+                {
+                  question: "What do I get exaclty?",
+                  answer:
+                    "You get an admin dashboard to add employees, track PTO, and approve supply requests, with the ability to assign roles and permissions to your team.",
+                },
+                {
+                  question: "Can I get a refund?",
+                  answer:
+                    "Yes, you can get a refund anytime if you are not satisfied with the product.",
+                },
+                {
+                  question: "Is your software mobile friendly?",
+                  answer:
+                    "Yes, PTO and Supply requests and approvals are mobile friendly. For assigning and adding employees youll want to use a desktop.",
+                },
+              ].map((qa) => (
+                <FAQListItem key={qa.question} qa={qa} />
+              ))
+            }
+          </ul>
         </div>
       </section>
     </main>
